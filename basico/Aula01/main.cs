5b6df008cb4155bc270fb53c680fc23f0fdb8b32
Ricardo Taverna <37935735+RicardoTaverna@users.noreply.github.com>
2022-12-17 12:43:10 -0300

    Add files via upload

---
+++ basico/Aula01/main.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Aula01
+{
+
+    class Progam
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World.");
+        }
+
+    }
+}
